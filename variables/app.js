8b3c5a74bb599f9a464b4ffb723a54c03c9c1c07
mlcoba <61705927+mlcoba@users.noreply.github.com>
2020-03-02 18:33:37 +0100

    new

---
+++ variables/app.js
@@ -0,0 +1,11 @@
+let number_1 = 1;
+let string_1 = "ahoj"; // promenna, ktera se bude po dobu behu menit - LET
+
+console.log(number_1);
+console.log(string_1);
+
+const number_2 = 1;
+const string_2 = "ahoj"; // promenna, ktera bude po celou dobu behu programu stejna - CONST
+
+console.log(number_2);
+console.log(string_2);
